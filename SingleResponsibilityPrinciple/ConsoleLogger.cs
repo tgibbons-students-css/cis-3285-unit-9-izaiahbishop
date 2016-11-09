073bf3d40111e158cbcb65ec24dcd867120d653e
ibishop <ibishop@css.edu>
2016-11-08 20:25:07 -0600

    Request  405 - "As a manager, I want to easily monitor the trading errors my traders make so I can provide them better training to avoid errors."

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using SingleResponsibilityPrinciple.Contracts;
+using System.IO;
 
 namespace SingleResponsibilityPrinciple
 {
@@ -9,7 +10,20 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><message>", message + "</message></log>" + args);
+            }
         }
+
+       /* private void LogMessage (string type, string message, params object[] args)
+        {
+            Console.WriteLine(message, args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>" + type + "</type><message>", message + "</message></log>" + args);
+            }
+        }*/
 
         public void LogInfo(string message, params object[] args)
         {
